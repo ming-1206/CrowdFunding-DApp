8f4aa6444fe78b9652012a5df49b7acef4495b2a
Ng Zi Ming <ngziming@Ngs-MacBook-Air.local>
2025-08-12 14:54:48 +0800

    client file changed

--- src/app/client.ts
+++ src/app/client.ts
@@ -3,9 +3,10 @@ import { createThirdwebClient } from "thirdweb";
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId =
-  process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || "your_client_id_here";
+  process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID ||
+  "7be0c197ff4f2aa245848e6aad4ba2a8";
 
-if (!clientId || clientId === "your_client_id_here") {
+if (!clientId || clientId === "7be0c197ff4f2aa245848e6aad4ba2a8") {
   throw new Error(
     "Please replace 'your_client_id_here' with your actual thirdweb client ID"
   );
