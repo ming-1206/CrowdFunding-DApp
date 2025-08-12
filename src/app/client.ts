af127053cd64cfedbc33127fad09eaa16300a7df
Ng Zi Ming <ngziming@Ngs-MacBook-Air.local>
2025-08-12 15:01:38 +0800

    client file changed

--- src/app/client.ts
+++ src/app/client.ts
@@ -6,12 +6,15 @@ const clientId =
   process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID ||
   "7be0c197ff4f2aa245848e6aad4ba2a8";
 
-if (!clientId || clientId === "7be0c197ff4f2aa245848e6aad4ba2a8") {
-  throw new Error(
-    "Please replace 'your_client_id_here' with your actual thirdweb client ID"
-  );
+// Only check and throw error in the browser (client-side)
+if (typeof window !== "undefined") {
+  if (!clientId || clientId === "7be0c197ff4f2aa245848e6aad4ba2a8") {
+    throw new Error(
+      "Please replace 'your_client_id_here' with your actual thirdweb client ID"
+    );
+  }
 }
 
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId,
 });
